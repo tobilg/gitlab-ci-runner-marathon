02df5d035dadb9cbcd89a884121db2d5767c6262
tobilg <tobilg@gmail.com>
2016-09-28 14:59:58 +0200

    Multiple fixes

--- register_and_run.sh
+++ register_and_run.sh
@@ -3,26 +3,29 @@
 set -eu
 
 # Ensure GITLAB_SERVICE_NAME (custom defined variable). We need this to discover the GitLab location
-if [ -z "$GITLAB_SERVICE_NAME" ]; then
+if [ -z ${GITLAB_SERVICE_NAME+x} ]; then
     echo "Need to set GITLAB_SERVICE_NAME to the service name of GitLab (e.g. _gitlab._tcp.marathon.mesos)"
     exit 1
 fi
 
 # Ensure REGISTRATION_TOKEN
-if [ -z "$REGISTRATION_TOKEN" ]; then
+if [ -z ${REGISTRATION_TOKEN+x} ]; then
     echo "Need to set REGISTRATION_TOKEN. You can get this token in GitLab -> Admin Area -> Overview -> Runners"
     exit 1
 fi
 
 # Ensure RUNNER_EXECUTOR
-if [ -z "$RUNNER_EXECUTOR" ]; then
-    echo "Need to set RUNNER_EXECUTOR. Please choose a alid executor, like 'shell' or 'docker' etc."
+if [ -z ${RUNNER_EXECUTOR+x} ]; then
+    echo "Need to set RUNNER_EXECUTOR. Please choose a valid executor, like 'shell' or 'docker' etc."
     exit 1
 fi
 
 # Check for RUNNER_CONCURRENT_BUILDS variable (custom defined variable)
-if [[ -v RUNNER_CONCURRENT_BUILDS ]]; then
-  sed -i -e "s|concurrent = 1|concurrent = ${RUNNER_CONCURRENT_BUILDS}|g" /etc/gitlab-runner/config.toml
+if [ -z ${RUNNER_CONCURRENT_BUILDS+x} ]; then
+    echo "Concurrency is set to 1"
+else
+    sed -i -e "s|concurrent = 1|concurrent = ${RUNNER_CONCURRENT_BUILDS}|g" /etc/gitlab-runner/config.toml
+    echo "Concurrency is set to ${RUNNER_CONCURRENT_BUILDS}"
 fi
 
 # Derive the Mesos DNS server ip address by getting the first nameserver entry from /etc/resolv.conf
@@ -47,6 +50,9 @@ export RUNNER_CACHE_DIR=${MESOS_SANDBOX}/cache
 
 # Set the RUNNER_WORK_DIR
 export RUNNER_WORK_DIR=${MESOS_SANDBOX}/cache
+
+# Create directories
+mkdir -p $RUNNER_BUILDS_DIR $RUNNER_CACHE_DIR $RUNNER_WORK_DIR
 
 # Print the environment for debugging purposes
 env
