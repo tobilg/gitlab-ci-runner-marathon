b08cbc19d2151a0ffd9c69141c2b40c0b9658545
tobilg <tobilg@gmail.com>
2016-10-12 12:26:24 +0200

    Fix for DOCKER_EXTRA_OPTS

--- register_and_run.sh
+++ register_and_run.sh
@@ -87,6 +87,16 @@ env
 
 # Launch Docker daemon
 # taken from https://github.com/mesosphere/jenkins-dind-agent/blob/master/wrapper.sh
+
+# Check for DOCKER_EXTRA_OPTS. If not present set to empty value
+if [ -z ${DOCKER_EXTRA_OPTS+x} ]; then
+    echo "==> Not using DOCKER_EXTRA_OPTS"
+    DOCKER_EXTRA_OPTS=
+else
+    echo "==> Using DOCKER_EXTRA_OPTS"
+    echo ${DOCKER_EXTRA_OPTS}
+fi
+
 echo "==> Launching the Docker daemon..."
 dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay $DOCKER_EXTRA_OPTS &
 
