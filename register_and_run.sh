7806494c7737358238263c176e65bb466fdac3bd
tobilg <tobilg@gmail.com>
2016-10-12 12:57:12 +0200

    Change back to overlay

--- register_and_run.sh
+++ register_and_run.sh
@@ -98,7 +98,7 @@ else
 fi
 
 echo "==> Launching the Docker daemon..."
-dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=aufs $DOCKER_EXTRA_OPTS &
+dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay $DOCKER_EXTRA_OPTS &
 
 # Wait for the Docker daemon to start
 while(! docker info > /dev/null 2>&1); do
