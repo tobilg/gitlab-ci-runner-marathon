343e41db6182d6a73db374a3ef22d8c869e69208
TobiLG <tobilg@gmail.com>
2017-03-23 17:43:41 +0100

    Remove the /ci from the CI_SERVER_URL, because the v9.0.0 of the runner only supports GitLab >= 9.0.0, which has another endpoint. Also, the --user flag was removed.

--- register_and_run.sh
+++ register_and_run.sh
@@ -68,13 +68,13 @@ if [ -z ${GITLAB_INSTANCE_URL+x} ]; then
 
     # Set the CI_SERVER_URL by resolving the Mesos DNS service name endpoint.
     # Environment variable GITLAB_SERVICE_NAME must be defined in the Marathon app.json
-    export CI_SERVER_URL=http://$(mesosdns-resolver --serviceName $GITLAB_SERVICE_NAME --server $MESOS_DNS_SERVER --portIndex 0)/ci
+    export CI_SERVER_URL=http://$(mesosdns-resolver --serviceName $GITLAB_SERVICE_NAME --server $MESOS_DNS_SERVER --portIndex 0)
 else
     # Display the GitLab instance URL discovery method
     echo "==> Using the GITLAB_INSTANCE_URL environment variable to set the GitLab instance URL"
 
-    # Set CI_SERVER_URL to the GITLAB_INSTANCE_URL and add the /ci endpoint
-    export CI_SERVER_URL=${GITLAB_INSTANCE_URL}/ci
+    # Set CI_SERVER_URL to the GITLAB_INSTANCE_URL
+    export CI_SERVER_URL=${GITLAB_INSTANCE_URL}
 fi
 
 # Derive the RUNNER_NAME from the MESOS_TASK_ID
@@ -137,4 +137,4 @@ trap _getTerminationSignal TERM
 gitlab-runner register
 
 # Start the runner
-gitlab-runner run --user=gitlab-runner --working-directory=${RUNNER_WORK_DIR}
+gitlab-runner run --working-directory=${RUNNER_WORK_DIR}
